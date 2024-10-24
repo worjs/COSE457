fe719b0a28749f57e3e802a4f75b8ae679e75f46
worjs <whworjs777@gmail.com>
2024-10-24 19:37:03 +0900

    main window and sidebars

---
+++ app/src/main/java/cose457/Main.java
@@ -0,0 +1,13 @@
+package cose457;
+
+import cose457.view.ScreenView;
+
+import javax.swing.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    ScreenView screenView = new ScreenView();
+    screenView.setVisible(true);
+  }
+}
